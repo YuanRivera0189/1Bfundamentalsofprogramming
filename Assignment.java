9de6cf45886faf3779ace5dbeedd4058edcc8e96
Yuan Kendrick Rivera <26ur0189@psu.edu.ph>
2026-09-02 20:46:29 +0800

    First commit

---
+++ Assignment.java
@@ -0,0 +1,31 @@
+public class Assignment {
+    public static void main(String[] args) {
+        // Variables for personal details
+        String firstName = "Yuan Kendrick";
+        //String will be used for First Name
+        String lastName = "Rivera";
+        //String will be used for the last name
+        String address = "Villasis, Pangasinan";
+        //String will be used for the address
+        String contactNumber = "0968-574-4505";
+        //String will be used for the adress
+        int age = 18;
+        //int will be used for the age
+        double gwa = 89;
+        //double will be used for the GWa
+
+        // Printing each variable on a new line
+        System.out.println("First Name: " + firstName);
+        //Line is used to print the First Name
+        System.out.println("Last Name: " + lastName);
+        //Line is used to print the Last Name
+        System.out.println("Address: " + address);
+        //Line is used to print the Address
+        System.out.println("Contact Number: " + contactNumber);
+        //Line is used to print the Contact Number
+        System.out.println("Age: " + age);
+        //Line is used to print the Age
+        System.out.println("GWa: " + gwa);
+        //Line is used to print the GWa
+    }
+}
